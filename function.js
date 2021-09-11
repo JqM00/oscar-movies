004097131aed494de71ae4666271954cf14be257
Maharishi Conceição <maharishi.conceicao@konecty.com>
2021-09-11 20:58:00 -0300

    add function to buttons

--- function.js
+++ function.js
@@ -1,26 +1,76 @@
-let anos = [];
+let year = [];
 
-for(let i = 1929; i < 2021; i++) {
-    anos.push(i);
+for (let i = 1929; i < 2021; i++) {
+  year.push(i);
 }
 
-index = parseInt(Math.random() * 100) % anos.length;
+index = parseInt(Math.random() * 100) % year.length;
 
-anos[index] 
-
+year[index];
 
 function discover() {
-    let inputAno = document.getElementById("random-year").value;
+  document.getElementById("random-year").value = year[index];
 }
 
+const category = [
+  "ACTOR",
+  "ACTRESS",
+  "ART DIRECTION",
+  "ASSISTANT DIRECTOR",
+  "BEST PICTURE",
+  "CINEMATOGRAPHY",
+  "COSTUME DESIGN",
+  "DANCE DIRECTION",
+  "DIRECTING",
+  "DOCUMENTARY",
+  "EDITING",
+  "ENGINEERING EFFECTS",
+  "FEATURE FILM",
+  "HONORARY AWARD",
+  "MAKE UP AND HAIRSTYLING",
+  "MUSIC SCORE",
+  "PRODUCTION",
+  "SHORT FILM",
+  "SONG",
+  "SOUND",
+  "SPECIAL AWARD",
+  "VISUAL EFFECTS",
+  "WRITING",
+];
+console.log(category[10]);
+
+var length = category.length;
+
+function randomCategory() {
+  var rand = Math.round(Math.random() * (length - 1));
+}
+
+var newCategory = category[rand];
+document.getElementById("random-category").innerHTML = newCategory;
+console.log(newCategory);
+
+// function randomYear() {
+//     let year = [];
+
+//     for (let i = 1929; i < 2021; i++) {
+//       year.push(i);
+//     }
+//   }
+
+//   index = parseInt(Math.random() * 100) % year.length;
+
+//   year[index];
+// //   console.log(year);
+// //   console.log(year[index], year.length)
+// //   return year[index];
+
+//   function discover() {
+//     document.getElementById("random-year").value = randomYear();
+//   }
 
 // var input = document.querySelector(#random-year);
 // var texto = input.value;
 // console.log(texto);
-
-
-
-
 
 // loop(year = 1929; yearEnd = 2021) {
 //     if (year = yearEnd) {
@@ -39,7 +89,6 @@ function discover() {
 //     document.write(frase);
 //     pulaLinha();
 // }
-
 
 // for (var year = 1929; year < 2021; i++) {
 //     mostra(2021[i]);
