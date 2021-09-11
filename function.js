a6ba2e347dbec7533fdbcbbe9093f389e9e91e16
Maharishi Conceição <maharishi.conceicao@konecty.com>
2021-09-11 18:43:58 -0300

    some adjusts and js

---
+++ function.js
@@ -0,0 +1,46 @@
+let anos = [];
+
+for(let i = 1929; i < 2021; i++) {
+    anos.push(i);
+}
+
+index = parseInt(Math.random() * 100) % anos.length;
+
+anos[index] 
+
+
+function discover() {
+    let inputAno = document.getElementById("random-year").value;
+}
+
+
+// var input = document.querySelector(#random-year);
+// var texto = input.value;
+// console.log(texto);
+
+
+
+
+
+// loop(year = 1929; yearEnd = 2021) {
+//     if (year = yearEnd) {
+//         exit loop;
+//     } else {
+//         year += 1930;
+//     }
+// }
+
+// function pulaLinha() {
+//     document.write("<br><br>");
+// }
+
+// function mostra(frase) {
+
+//     document.write(frase);
+//     pulaLinha();
+// }
+
+
+// for (var year = 1929; year < 2021; i++) {
+//     mostra(2021[i]);
+// }
